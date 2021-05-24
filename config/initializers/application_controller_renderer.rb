1be0c6a99afb6154aa4a60a862da831c6bb898e0
a-wtnbe <ayawatanabe714@gmail.com>
2021-05-24 13:57:28 +0000

    3章end

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b08c3457c021fa4de6d0e7838a5ae285287ab473cc3d9d82a44be93c082827233d5db3c07f6bd9e6c144285303fdfe8257619ec560d71785df1ef187465584c4'
